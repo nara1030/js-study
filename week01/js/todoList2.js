3a3271b84bd85516608e6256862e2c2131a79f1e
hjeom <nara1030@naver.com>
2021-07-11 10:53:40 +0900

    .

---
+++ week01/js/todoList2.js
@@ -0,0 +1,110 @@
+/**
+ * Requirement
+ * 
+ * 1. 할 일 추가/삭제
+ * 2. 새로고침 시 유지
+ */
+
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.querySelector('#todo-form input:first-child');
+const todoSubmit = document.querySelector('#todo-form button');
+const ul = document.querySelector('#todo-list');
+
+// JSONArray
+let todoList = [];
+
+function onAddBtnClicked(event) {
+    event.preventDefault();
+    
+    const itemText = todoInput.value;
+    if (itemText === "") { // html property not working 
+        console.log('Please write what to do');
+        return;
+    }
+
+    // 1. store data to array
+    const item = addItem(itemText);
+    // 2. draw data to screen
+    drawItem(item);
+    // 3. reflect data to localStorage
+    saveItem();
+    // 4. refresh input text
+    todoInput.value = "";
+    // 5. remove hidden property
+    ul.classList.remove('hidden');
+}
+
+function addItem(itemText) {
+    const newItem = {};
+    newItem.text = itemText;
+    // add unique property(id) for deleting item
+    newItem.id = Date.now();
+    console.log(newItem.id, typeof newItem.id);
+    todoList.push(newItem);
+    
+    return newItem;
+}
+
+function drawItem(item) {
+    const li = document.createElement('li');
+    li.id = item.id;
+    const span = document.createElement('span');
+    li.appendChild(span);
+    span.innerText = item.text;
+    const button = document.createElement('button');
+    button.addEventListener('click', onRemoveBtnClicked);
+    li.appendChild(button);
+    button.innerText = '❌'; // window + .
+    ul.appendChild(li);
+}
+
+function saveItem() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+todoForm.addEventListener('click', onAddBtnClicked);
+
+function onRemoveBtnClicked(event) {
+    const itemId = event.target.parentNode.id;
+    removeItem(itemId);
+}
+
+function removeItem(itemId) {
+    console.log(itemId, typeof itemId);
+
+    // 1. clear item from screen
+    const liToBeRemoved = ul.querySelector(String(itemId));
+    console.log(liToBeRemoved);
+    // liToBeRemoved.remove();
+    
+    // 2. remove item from json array
+    todoList = todoList.filter(element => {
+        // console.log(`${typeof element.id} : ${typeof parseInt(itemId)}`);
+        return element.id !== parseInt(itemId);
+    });
+
+    // 3. remove item from localStorage
+    // localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+// when user refresh the screen
+function initiate() {
+    const savedTodoList = localStorage.getItem('todoList');
+    if (savedTodoList === null) {
+        return;
+    }
+
+    // 1. save todoList array using localStorage data
+    // this is why we declare todoList using 'let'
+    todoList = JSON.parse(savedTodoList);
+    
+    // 2. draw items to screen
+    todoList.forEach(element => {
+        drawItem(element);
+    });
+
+    // 3. remove hidden property
+    ul.classList.remove('hidden');
+}
+
+initiate();
--- week01/week_01.md
+++ week01/week_01.md
@@ -15,6 +15,38 @@
     * [버블링과 캡처링](https://ko.javascript.info/bubbling-and-capturing)
 * [How to pass arguments to addEventListener listener function?](https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function)
 
+- - -
+다시 구현(2021.07.11)
+
+1. html, js, css 각 역할 분리 필요
+    * html 속성 이용하면 js 유효성 검사 등 최소화 가능
+    * js에서 style 변경 말고, class를 통해 수정
+    * class 추가 시 className 대신 classList를 통하면 기존 적용 값 유지
+2. js에서 html을 가져오는 방법으로 `querySelector()`를 권장
+    * `getElementById()`나 `getElementsByClassName()`과 다르게 css 선택자로 요소 선택 가능
+3. 기본적으로 `const`, 필요시 `let` 사용
+    * 두 개 이상 하드 코딩 시 `const`로 선언하면 오타 검출 가능
+4. 이벤트를 추가하는 방법에는 두 가지가 있으나, `addEventListener()` 권장
+    * .
+    * 자바스크립트 파라미터. event 관례.
+    * `event.prevent...`는 html 기본 동작 방지
+    * `forEach` 시 파라미터 item
+5. input 외부 form 태그로 감싸야..
+6. localStorage에는 `string`만 저장
+    * localStorage에 해당 key에 해당하는 값이 없는 경우 `null` 반환
+7. `console.log()` 출력 시 `null`은 `null`로, `""`는 안 찍힘
+    * `console.dir()`
+8. 기타
+    * alert창 커스터마이징 불가 단점, 모달 사용
+    * 함수 정의 내부 함수 호출
+    * `appendChild()` 이후 `innerText` 설정
+
+- - -
+참고
+
+* [노마드 코더 강의](https://nomadcoders.co/javascript-for-beginners/lectures/2899)
+* [깃허브](https://github.com/nomadcoders/javascript-for-beginners)
+
 ##### [목차로 이동](#목차)
 
 ## ETC
